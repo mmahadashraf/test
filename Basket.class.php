904cd58d283b4b71a5cd6f24f962d07ea3306aa5
mmahadashraf <54333114+mmahadashraf@users.noreply.github.com>
2019-08-21 12:50:17 +0500

    test and removed bugs in total calculations

--- Basket.class.php
+++ Basket.class.php
@@ -5,9 +5,9 @@ class Basket{
 	private $minDeliveryRuleRate;	//minimum shipping rule rate
 	private $maxDeliveryRuleTotal;	//maximum shipping rule order price
 	private $maxDeliveryRuleRate;	//maximum shipping rule rate
-    	private $productsList;	//ordered products list to add/remove (with quantity) 
+    	private $productsList;	//ordered products list to add/remove
 	private $productsListTotal;	//ordered products total
-	private $shippingCost;	//shipping cost on order total as per rules
+
   
 //initialisation
     public function __construct(){
@@ -20,42 +20,28 @@ class Basket{
 	$this->minDeliveryRuleRate = 4.95;  
         $this->maxDeliveryRuleTotal = 90;
 	$this->maxDeliveryRuleRate = 2.95;   
-	$this->shippingCost = 0;	//for convenience in invoice display
-	$this->productsListTotal = 0;
-        $this->productsList = $this->catalog = [	//assocaitive array created for convenience in invoice display
-        	'R01' => 0,
-		'G01' => 0,
-            	'B01' => 0            
-        ];
-	    //alternate approach
-	    /*
-	    	$this->productsList = [];	
-	    */
+	$this->productsListTotal = 0;	 
+    	$this->productsList = [];	     
+	
     }
 	
 //add product
     public function addProduct($productCode) {  
-        //due to the convenience approach
-	    $this->productsList[$productCode]+=1;
-	//alternate approach
-	/*    $this->productsList[] = $productCode;*/
+
+        $this->productsList[] = $productCode;
 
     }
 	
 //remove product	
     public function removeProduct($productCodeParam) {  
-        //due to the convenience approach
-	if($this->productsList[$productCodeParam]>=1)
-		$this->productsList[$productCodeParam]-=1;
-	else
-		$this->productsList[$productCodeParam]=0;
-	 //alternate approach
-	    /*foreach($this->productsList as $index => $productCode) {
+
+	    foreach($this->productsList as $index => $productCode) {
+
             if($productCode == $productCodeParam) {
-                unset($this->products[$index]);
+                unset($this->productsList[$index]);
                 break;
             }
-        }*/
+        }
 	    
     }
 	
@@ -66,25 +52,9 @@ class Basket{
 	
 //calculate total of basket	
     public function getTotal() {  
-        $this->productsListTotal = 0;        
-	    //due to the convenience approach
-	foreach($this->productsList as $productCode => $qty) {			
-           
-            if($productCode != 'R01'){
-			$this->productsListTotal += ($this->catalog[$productCode]*$qty);                    
-	    }else{
-		$quo=$qty/2;
-		$rem=$qty%2;
-		if($rem==0){	//for even quantity: total for this product will be (quotient*price + quotient*half price)
-			  $this->productsListTotal += (($quo*$this->catalog[$productCode]) + ($quo *($this->catalog[$productCode]/2));
-		}else {	//for even quantity: total for this product will be (sum of quotient & remainder*price) + (quantity - sum of quotient & remainder)*half price
-			  $this->productsListTotal += (($quo + $rem)*$this->catalog[$productCode])+(($qty - ($quo + $rem))*($this->catalog[$productCode]/2));
-		}				      
-	    }
-         }
-//alternate approach 
-/*
-     $redWidgetExists = false;
+        $this->productsListTotal = 0;        	
+
+        $redWidgetExists = false;
         if(count($this->productsList) == 0) {
             return 0;
         }
@@ -92,18 +62,18 @@ class Basket{
            
             if($product != 'R01'){ //for all other products simply add to sub total
 				$this->productsListTotal += $this->catalog[$product];                    
-			}else{	//for red widgets for alternate piece charge half of the price usong redwidget flag which resets once set true
+			}else{	//for red widgets for alternate piece charge half of the price using redwidget flag which resets once set true
 				$this->productsListTotal += (($redWidgetExists)?($this->catalog[$product] / 2):$this->catalog[$product]);					 				
 				$redWidgetExists = !$redWidgetExists;             
 			}
         }
-*/						       
+						       
         //shipping rules calculations
-        if($this->productsListTotal < $this->minDeliveryRuleTotal) { 
-            $this->shippingCost = $this->minDeliveryRuleRate;
-        } else if($this->productsListTotal < $this->maxDeliveryRuleTotal) { 
-            $this->shippingCost = $this->maxDeliveryRuleRate;
+        if($this->productsListTotal <= $this->minDeliveryRuleTotal) { 
+            $this->productsListTotal += $this->minDeliveryRuleRate;
+        } else if($this->productsListTotal <= $this->maxDeliveryRuleTotal) { 
+            $this->productsListTotal += $this->maxDeliveryRuleRate;
         }
-        return round($this->productsListTotal+$this->shippingCost, 2);	//rounding to 2 decimals as shown in input samples
+        return round($this->productsListTotal, 2);	//rounding to 2 decimals as shown in input samples
     }//end function getTotal
 }//end class
